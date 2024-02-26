3992e115d56ff5579a4eb8501919084a8cd0d5c5
Walt9819 <walter22r@icloud.com>
2024-02-25 23:16:26 -0600

    Display and iterate in the template through the database data

--- STREAMING/models.py
+++ STREAMING/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    release_year = models.IntegerField()
+    genre = models.CharField(max_length=100)
+    director = models.CharField(max_length=100)
+    description = models.TextField(null=True, default=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.release_year})"
--- STREAMING/views.py
+++ STREAMING/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponseBadRequest
+
+from STREAMING.models import Movie
 
 # Main site view
 def movies(request):
-    return render(request, 'STREAMING/movies.html')
+    # If request method is GET, render the movies
+    if request.method == "GET":
+        # Get all movies from the database
+        movies = Movie.objects.all()
+        return render(request, 'STREAMING/movies.html', {'movies': movies})
+    else:
+        return HttpResponseBadRequest("Invalid request method")
