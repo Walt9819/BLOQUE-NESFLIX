3ed014cd8ecad057a951aef2ebe931ebfd4b6dd1
Walt9819 <walter22r@icloud.com>
2024-02-25 22:17:55 -0600

    Add app to installed apps and display the template

--- STREAMING/views.py
+++ STREAMING/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Main site view
 def movies(request):
-    return HttpResponse("Welcome to NESFLIX! The best streaming service in the world!")
+    return render(request, 'STREAMING/movies.html')
